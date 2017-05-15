4e28aeeda4e9d78cee6288b6262ac04340a136a0
= <alexininternet@gmail.com>
2017-05-16 00:05:43 +0300

    Complete memorize part of game

--- js/main.js
+++ js/main.js
@@ -1,15 +1,65 @@
-		var i = 0, images = ["images/emma.jpg",
-			"images/albert.jpg",
-			"images/portrait.jpg"];
+var quizData = [
+  { id: 0,
+    image: "images/emma.jpg",
+    name: "Emma"},
+  { id: 1,
+    image: "images/albert.jpg",
+    name: "Albert" },
+  { id: 3,
+    image: "images/portrait.jpg",
+    name: "Natalie"}
+];
+var index = -1;
+var $start = $('#start');
+var $end = $('#end');
+var $image = $('#image');
+var $text = $('#text');
+var $name = $('#name');
+var $prev = $('#previous');
+var $next = $('#next');
 
-		function mySlide(param) {
-			if (param === 'next') {
-				i++;
-				if (i === images.length) { i = images.length - 1; }
-			} else {
-				i--;
-				if (i < 0) { i = 0; }
-			}
+function update(){
+  if(index===-1){
+    $start.show();
+    $text.hide();
+    load('','');
+    return;
+  }
+  if(index===0){
+    $start.hide();
+    $text.show();
+  }
+  if(index===quizData.length-1){
+    $end.hide();
+    $text.show();
+  }
+  if(index===quizData.length){
+    $end.show();
+    $text.hide();
+    load('','');
+    return;
+  }
+  load(quizData[index].image,quizData[index].name);
+}
 
-			document.getElementById('slide').src = images[i];
-		}
+function load(image,name){
+  $image.attr('src',image);
+  $name.text(name);
+}
+
+$next.click(function(){
+  console.log(index);
+  if(index===quizData.length){
+    return;
+  }
+  index++;
+  update();
+});
+
+$prev.click(function(){
+  if(index===-1){
+    return;
+  }
+  index--;
+  update();
+});

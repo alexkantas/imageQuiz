93acf72dfea4862bbfd23c9acd7f84eba8ef0d51
= <alexininternet@gmail.com>
2017-05-18 21:21:56 +0300

    Add fontEnd for delete action in admin dashboard

--- js/main.js
+++ js/main.js
@@ -73,25 +73,33 @@ function loadFaces(url) {
 
 function showAll(url) {
   var $questionList = $("#questionList");
-  var $actionButton = $('.secondary-content');
+  var $actionButtons;
+  $('#delete-modal').modal();
 
   var questionBlockTemplate = '<li class="collection-item avatar">' +
         '<img src="{image}" alt="" class="circle">' +
         '<span class="title">Id <span>{id}</span></span>' +
         '<p>{name}</p>' +
         '<p>level <span>{level}</span></p>' +
-        '<a id="{id}" questionName="{name}" class="secondary-content"><i class="material-icons">delete</i></a>' +
+        '<a quetionId="{id}" questionName="{name}" class="secondary-content delete-button"><i class="material-icons">delete</i></a>' +
         '</li>';
 
   $.getJSON(url, function (data) {
     data.forEach(function (question) {
       questionBlock = questionBlockTemplate
-      .replace('{image}', question.image)
-      .replace('{id}', question.id)
-      .replace('{name}', question.name)
-      .replace('{level}', question.level);
+      .replace(/{image}/g, question.image)
+      .replace(/{id}/g, question.id)
+      .replace(/{name}/g, question.name)
+      .replace(/{level}/g, question.level);
       $questionList.append(questionBlock);
     });
+    $actionButtons = $('.delete-button');
+    $actionButtons.click(function () {
+    var questionId = $(this).attr('quetionId');
+    $('#confirm-message').text('Delete the image with id ' + questionId + ' and name ' + $(this).attr('questionName') + ' ?');
+    $('#inputdata').attr('value',questionId);
+    $('#delete-modal').modal('open');
+  });
   })
     .fail(function () {
       $questionList.text("An error occupied :( Please try to reload the page!");
@@ -99,8 +107,4 @@ function showAll(url) {
     .always(function(){
       $('#preloader').hide();
     });
-
-  $actionButton.click(function () {
-    alert('Hi! id is ' + $(this).attr('notificationId'));
-  });
 }
